8d585677b3c97d72906c53ee72830b03bf409eb9
jsaliba <justin.saliba@ricston.com>
2013-06-11 14:54:36 +0200

    Added DropCollectionTestCase test case

---
+++ src/test/java/org/mule/module/mongo/automation/testcases/DropCollectionTestCase.java
@@ -0,0 +1,54 @@
+package org.mule.module.mongo.automation.testcases;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+public class DropCollectionTestCase extends MongoTestParent {
+
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("createCollection");
+			MessageProcessor flow = lookupFlowConstruct("create-collection");
+			flow.process(getTestEvent(testObjects));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Category({SmokeTests.class, SanityTests.class})
+	@Test
+	public void testDropCollection() {
+		
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("dropCollection");			
+			MessageProcessor flow = lookupFlowConstruct("drop-collection");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+			flow = lookupFlowConstruct("exists-collection");
+			response = flow.process(getTestEvent(testObjects));
+			
+			Object payload = response.getMessage().getPayload();
+			assertFalse((Boolean)payload);
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	
+}
